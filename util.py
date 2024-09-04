cb6544b3ed895051c863a26e1b56106657a0c7e2
Valer100 <122545522+Valer100@users.noreply.github.com>
2024-09-04 19:27:17 +0300

    Small edit

--- util.py
+++ util.py
@@ -85,7 +85,7 @@ def delete_remnants():
     subprocess.call(f"mkdir \"{working_folder}\\shortcut\"", shell = True)
 
 def post_create_shortcut():
-    subprocess.call(f"explorer \"{working_folder}\\shortcut\"", shell = True)
+    subprocess.call(f"start \"{working_folder}\\shortcut\"", shell = True)
 
     ctypes.windll.user32.MessageBoxW(
         None,
