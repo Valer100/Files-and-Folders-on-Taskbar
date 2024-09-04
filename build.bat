6acf788c64d74de3f0297d76c5f27a87bbe29107
Valer100 <122545522+Valer100@users.noreply.github.com>
2024-09-04 13:13:17 +0300

    Create building script

---
+++ build.bat
@@ -0,0 +1,35 @@
+@echo off
+title Building Files ^& Folders on taskbar...
+
+echo (1/4) Deleting temporary files...
+echo.
+
+rmdir /q /s "build"
+rmdir /q /s "dist"
+del /f /q "fnf_on_taskbar.spec"
+
+echo.
+echo (2/4) Installing dependencies...
+echo.
+
+python -m pip install -r requirements.txt
+python -m pip install pyinstaller
+
+echo.
+echo (3/4) Building with PyInstaller...
+echo.
+
+python -m PyInstaller main.pyw --onedir --icon icon.ico --version-file "version.txt" --name "fnf_on_taskbar" --add-data "separators;./separators/" --add-data "icon.ico;."
+
+echo .
+echo (4/4) Deleting temporary files...
+echo.
+
+rmdir /q /s "build"
+del /f /q "fnf_on_taskbar.spec"
+ren "dist" "build"
+
+echo.
+echo Files ^& Folders on taskbar has been successfully built.
+echo Press any key to exit...
+timeout /t -1 > NUL
