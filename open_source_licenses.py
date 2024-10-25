318309e4b115f24497f343cafbafffaac39ee01c
Valer100 <122545522+Valer100@users.noreply.github.com>
2024-10-25 18:36:49 +0300

    Make `Text` widget support dark mode

--- open_source_licenses.py
+++ open_source_licenses.py
@@ -13,7 +13,11 @@ def show():
 
     ttk.Label(window, text = strings.lang.open_source_licenses, font = ("Segoe UI Semibold", 17)).pack(anchor = "w", pady = (8, 16))
 
-    licenses_text = ScrolledText(window, width = "90", height = 30, wrap = "word")
+    licenses_text = ScrolledText(window, width = "90", height = 30, wrap = "word", background = custom_ui.entry_bg,
+                                 foreground = custom_ui.fg, selectbackground = custom_ui.entry_select,
+                                 selectforeground = "#ffffff", highlightthickness = 1, relief = "solid",
+                                 highlightbackground = custom_ui.entry_bd, highlightcolor = custom_ui.entry_bd,
+                                 border = 0, font = ("Consolas", 10))
     licenses_text.pack(pady = (0, 16))
 
     licenses_text.insert("1.0", open(util.internal + "OPEN_SOURCE_LICENSES.txt", "r", encoding = "utf8").read())
