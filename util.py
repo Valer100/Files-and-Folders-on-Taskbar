a030952e8ce921a87b60f31d0f28686d1751e65d
Valer100 <122545522+Valer100@users.noreply.github.com>
2024-10-26 15:32:03 +0300

    Add function to sanitize file names

--- util.py
+++ util.py
@@ -139,3 +139,19 @@ def get_folder_icon(folder_path: str) -> str:
             folder_icon = (_folder_icon.replace("," + str(_folder_icon_index), ""), _folder_icon_index)
     
     return folder_icon
+
+def sanitize_filename(name: str) -> str:
+    illegal_characters = ["\\", "/", ":", "*", "?", "\"", "<", ">", "|"]
+
+    illegal_names = ["nul", "con", "prn", "aux", "com1", "com2", "com3", 
+                     "com4", "com5", "com6", "com7", "com8", "com9", "lpt1",
+                     "lpt2", "lpt3", "lpt4", "lpt5", "lpt6", "lpt7", "lpt8",
+                     "lpt9"]
+    
+    for character in name:
+        if character in illegal_characters: name.replace(character, "_")
+
+    for illegal_name in illegal_names:
+        if name == illegal_name: name = illegal_name + "_"
+
+    return name
