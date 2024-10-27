f7b8b79d93f2ccb4e4ad1a62db7a926fb697d982
Valer100 <122545522+Valer100@users.noreply.github.com>
2024-10-27 20:00:00 +0200

    Small redesign

--- separator_wizard.py
+++ separator_wizard.py
@@ -1,4 +1,4 @@
-import tkinter as tk, util, strings, custom_ui
+import tkinter as tk, util, strings, custom_ui, os
 from tkinter import ttk
 
 strings.load_language(open(util.user_preferences + "\\language", "r").read())
@@ -15,11 +15,27 @@ def show():
     ttk.Label(window, text = strings.lang.pin_separator, font = ("Segoe UI Semibold", 17)).pack(anchor = "w")
     ttk.Label(window, text = strings.lang.choose_a_separator_type).pack(anchor = "w", pady = 8)
 
-    ttk.Radiobutton(window, text = strings.lang.vertical_line, variable = separator_type, value = "vertical").pack(anchor = "w")
-    ttk.Radiobutton(window, text = strings.lang.horizontal_line, variable = separator_type, value = "horizontal").pack(anchor = "w")
-    ttk.Radiobutton(window, text = strings.lang.transparent, variable = separator_type, value = "transparent").pack(anchor = "w")
+    separator_types = ttk.Frame(window)
+    separator_types.pack(pady = 8)
 
-    pin_separator_btn = ttk.Button(window, text = strings.lang.create_the_shortcut, default = "active", command = lambda: util.create_separator_shortcut(separator_type.get()))
-    pin_separator_btn.pack(fill = "x", pady = (16, 8))
+    separator_vertical = tk.PhotoImage(file = util.internal + "separators/preview/separator_vertical.png")
+    separator_horizontal = tk.PhotoImage(file = util.internal + "separators/preview/separator_horizontal.png")
+    transparent = tk.PhotoImage(file = util.internal + "separators/preview/transparent.png")
+
+    vertical = ttk.Button(separator_types, width = 100, image = separator_vertical, compound = "top",
+                          text = strings.lang.vertical_line, command = lambda: util.create_separator_shortcut("vertical"))
+    vertical.pack(side = "left")
+    vertical.configure(pady = 8)
+
+    horizontal = ttk.Button(separator_types, width = 100, image = separator_horizontal, compound = "top",
+                            text = strings.lang.horizontal_line, command = lambda: util.create_separator_shortcut("horizontal"))
+    horizontal.pack(side = "left", padx = (8, 0))
+    horizontal.configure(pady = 8)
+
+    transparent_b = ttk.Button(separator_types, width = 100, image = transparent, compound = "top",
+                               text = strings.lang.transparent, command = lambda: util.create_separator_shortcut("transparent"))
+    transparent_b.pack(side = "left", padx = (8, 0))
+    transparent_b.configure(pady = 8)
 
     window.focus_set()
+    window.mainloop()
